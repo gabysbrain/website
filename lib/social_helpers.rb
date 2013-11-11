540dadf709a2deb32f5944263c01afaa9f2a2656
Thomas Torsney-Weir <torsneyt@gmail.com>
2013-11-11 12:51:13 +0100

    add social links

---
+++ lib/social_helpers.rb
@@ -0,0 +1,23 @@
+
+module SocialHelpers
+  def email_url
+    'mailto:thomas.torsney-weir@univie.ac.at'
+  end
+
+  def facebook_url
+    'https://www.facebook.com/torsneyt'
+  end
+
+  def foursquare_url
+    'https://foursquare.com/gabysbrain'
+  end
+
+  def github_url
+    'https://github.com/torsneyt'
+  end
+
+  def twitter_url
+    'https://twitter.com/gabysbrain'
+  end
+end
+
