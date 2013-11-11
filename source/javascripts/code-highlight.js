cab2096e74fe18c49b6dfc6b6a1856fb8dc5edcf
Thomas Torsney-Weir <torsneyt@gmail.com>
2013-11-11 12:09:36 +0100

    add line numbering support

--- source/javascripts/code-highlight.js
+++ source/javascripts/code-highlight.js
@@ -2,6 +2,9 @@
 $(document).ready(function() {
   $('pre code').each(function() {
     $(this).parent().addClass('prettyprint');
+    if($(this).hasClass('linenums')) {
+      $(this).parent().addClass('linenums');
+    }
   });
   prettyPrint();
 });
