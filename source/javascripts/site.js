fb005b2ba025def8898432bd02aa4e84d09a95af
Thomas Torsney-Weir <torsneyt@gmail.com>
2014-01-07 16:27:03 +0100

    site seems to be working but I need to play with the fonts

--- source/javascripts/site.js
+++ source/javascripts/site.js
@@ -2,6 +2,7 @@
 //= require foundation/js/foundation
 //= require vendor/prettify.js/prettify
 //= require vendor/responsive-nav/responsive-nav
+//= require_directory .
 
 $(document).foundation();
 
