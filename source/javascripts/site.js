5b8a499222080a8cb94afa505eaced413ff62f7c
Thomas Torsney-Weir <torsneyt@gmail.com>
2014-07-23 12:27:03 +0200

    manage responsive-nav with bower

--- source/javascripts/site.js
+++ source/javascripts/site.js
@@ -1,6 +1,6 @@
 //= require modernizr/modernizr
 //= require jquery/dist/jquery
-//= require vendor/responsive-nav/responsive-nav
+//= require responsive-nav/responsive-nav
 //= require _foundation
 //= require _code-highlight
 //= require _site-nav
