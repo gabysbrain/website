351ca4fa29095e977a2a9bf58121908ecd0a6114
Thomas Torsney-Weir <torsneyt@gmail.com>
2016-01-13 13:01:29 -0800

    update website link and class for the link

--- lib/link_helpers.rb
+++ lib/link_helpers.rb
@@ -4,8 +4,8 @@ module LinkHelpers
     #puts sitemap.current_resource
     # TODO: get page file from the sitemap or something
     curpath = current_page.source_file.scan(/\/source\/.*/)[0]
-    link_to('Edit this page', "https://github.com/gabysbrain/gabysbrain.github.io/#{curpath}",
-            :id => 'edit-link')
+    link_to('Edit this page', "https://github.com/gabysbrain/website/#{curpath}",
+            :class => 'edit-page-link')
   end
 end
 
