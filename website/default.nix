6df7134bdf3584cc19028c1a2ccbe0e6dab02029
Thomas Torsney-Weir <torsneyt@gmail.com>
2017-12-23 16:39:18 +0100

    basic hakyll layout

---
+++ website/default.nix
@@ -0,0 +1,27 @@
+#!/usr/bin/env nix-build
+
+{ pkgs ? import <nixpkgs> {} }:
+let generator = pkgs.stdenv.mkDerivation {
+      name = "website-generator";
+      src = ./generator;
+      phases = "unpackPhase buildPhase";
+      buildInputs = [
+        (pkgs.haskellPackages.ghcWithPackages (p: with p; [ hakyll ]))
+      ];
+      buildPhase = ''
+        mkdir -p $out/bin
+        ghc -O2 --make site.hs -o $out/bin/generator
+      '';
+    };
+in pkgs.stdenv.mkDerivation {
+     name = "website-site";
+     src = ./site;
+     phases = "unpackPhase buildPhase";
+     buildInputs = [ generator ];
+     buildPhase = ''
+       #LANG=en_US.UTF-8 generator build
+       generator build
+       mkdir $out
+       cp -r _site/* $out
+     '';
+   }
